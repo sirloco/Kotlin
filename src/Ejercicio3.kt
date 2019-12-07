cb4ce1ab0e0bcff0923c325ab4b4e278ccf4c06b
santi <sirlocus66@gmail.com>
2019-12-07 13:28:00 +0100

    ejercicio 3

--- src/Ejercicio3.kt
+++ src/Ejercicio3.kt
@@ -1,53 +1,35 @@
 fun main() {
 
     print("Escribe un número: ")
-
+    //hace un cast a enteros
     var n: Int = readLine()?.toInt() as Int
+    //calcula el número de vueltas totales de la figura
+    var vueltas = n * 2 - 1
+    //el numero de vueltas es el mismo que los asteriscos por fila
+    var asteriscos = vueltas
+    //inicializa los espacios a 0
+    var espacios = 0
+    //prepara la bandera para el cambio de orientacion
+    var cambio = false
 
-    var asteriscos: Int = 1
-
-    for (i in 2..n){
-
-        asteriscos += 2
-    }
-
-    var reverso: Int = n
-
-//////////////////////////////////////////
-    var vueltas: Int = asteriscos
-    var espacios: Int = 1
+    //empieza la fiesta
     for (i in 1..vueltas) {
-
+        //primero pinta los espacios de cada fila
         for (j in 1..espacios) {
             print(" ")
         }
-
+        //despues pinta los asteriscos
         for (j in 1..asteriscos) {
             print("*")
         }
-
-        if (asteriscos == 1){
-
-        }
-        espacios ++
-        asteriscos -= 2
+        //salto de linea
         println()
-    }
-////////////////////////////////////////////
-    var asteriscosReverso: Int = 1
-
-    for (i in 1..n) {
-
-        for (j in 1..reverso) {
-            print(" ")
-        }
-
-        for (j in 1..asteriscosReverso) {
-            print("*")
-        }
-        reverso--
-        asteriscosReverso += 2
-        println()
+        //si llega a pintar un asterisco solo hace cambio de vuelta
+        if (asteriscos == 1)
+            cambio = true
+        //oeprador ternario que va asignado los asteriscos y espacios
+        asteriscos = if (cambio) asteriscos + 2 else asteriscos - 2
+        espacios = if (cambio) espacios - 1 else espacios + 1
     }
 
 }
